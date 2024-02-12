85d8ab3cecf6a3ee990b3dea63bc49aa91a7906c
AsokanCB <cb.asokan@gmail.com>
2024-02-12 14:33:36 +0530

    Episode3

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div',{id:'parent'},
-    [
-        React.createElement("div",{id:'child'},
-    [React.createElement("h1",{id:'heading'},"I am H1 tag"),React.createElement("h2",{id:'heading'},"I am H2 tag")]),
-    React.createElement("div",{id:'child1'},
-    [React.createElement("h1",{id:'heading'},"I am H1 tag"),React.createElement("h2",{id:'heading'},"I am H2 tag")])
-    ]
-)
+//React.createElement ==> ReactElement - JS Object ==> HTMLElement (render)
+
+//const headingElement = React.createElement("div",{id:'heading'},"Namaste Raect");
+
+//JSX ==> Babel transpiles it to React.createElement ==> ReactElement - JS Object ==> HTMLElement (render)
+
+const TitleComponent = ()=>(<h1 id="heading">Converting element to component and doing Component Composition</h1>);
+
+const HeadingComponent =()=>{
+
+return(
+    <>
+    {TitleComponent()}
+    <TitleComponent/>
+    <h1 className="heading">Namaste Raect using Functional Component </h1>;
+    </>
+) 
+};
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(parent);
+root.render(<HeadingComponent />);
